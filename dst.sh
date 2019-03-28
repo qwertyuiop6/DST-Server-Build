ff8674995589ae0902405a156246f00ad4bfdb9d
qwertyuiop6 <super771783057@foxmail.com>
2019-03-28 21:05:26 +0800

    add init script and update admin script

--- dst.sh
+++ dst.sh
@@ -8,7 +8,7 @@ dst_dir=(${master} ${cave})
 stop(){
 	ps -ef|grep dontstarve|awk '{print $2}'|xargs kill -9
 	if [[ -z `ps -ef | grep -v grep |grep -v "dst.sh"|grep "dontstarve"|sed -n '1P'|awk '{print $2}'` ]]; then
-		echo -e "\033[32m ##: Dst server stop sucsess! \033[0m"
+		echo -e "\033[32m ##: 饥荒服务器停止成功!! \033[0m"
 	fi
 }
 
@@ -18,7 +18,7 @@ del(){
 	do
 		if [[ -d ${i}"save" ]]; then
 			rm -r ${i}"save"&&rm -r `find ${i} -name "*.txt"` && rm -r ${i}"backup"
-			echo -e "\033[32m ##: ${i}'s file already delete! \033[0m"
+			echo -e "\033[32m ##: ${i}'s 文件已经删除! \033[0m"
 		fi
 	done
 }
@@ -29,19 +29,11 @@ goMaster(){
 	if [[ -z `ps -ef | grep -v grep |grep -v "dst.sh"|grep "Master"|sed -n '1P'|awk '{print $2}'` ]]; then
 		screen -dm sh overworld.sh && if [[ `echo $?` -eq 0 ]]; 
 		then
-			echo -e "\033[36m ##: Master sucsess to start! \033[0m"
+			echo -e "\033[36m ##: 地上成功启动... \033[0m"
 		fi
 	else
-		echo -e "\033[31m !!! Master already started!!! \033[0m"
+		echo -e "\033[31m !!! 地上正在运行中!!! \033[0m"
 	fi
-	
-	# if [[ -z `ps -ef | grep -v grep |grep -v "dst.sh"|grep "Caves"|sed -n '1P'|awk '{print $2}'` ]]; then
-	# 	screen -d sh cave.sh && if [[ `echo $?` -eq 0 ]]; then
-	# 		echo -e "\033[32m ##: Cave sucsess to start! \033[0m"
-	# 	fi
-	# else
-	# 	echo -e "\033[31m !!! Cave already started!!! \033[0m"
-	# fi
 	
 }
 
@@ -50,16 +42,21 @@ goCaves(){
 	
 	if [[ -z `ps -ef | grep -v grep |grep -v "dst.sh"|grep "Caves"|sed -n '1P'|awk '{print $2}'` ]]; then
 		screen -dm sh cave.sh && if [[ `echo $?` -eq 0 ]]; then
-			echo -e "\033[36m ##: Cave sucsess to start! \033[0m"
+			echo -e "\033[36m ##: 洞穴启动成功... \033[0m"
 		fi
 	else
-		echo -e "\033[31m !!! Cave already started!!! \033[0m"
+		echo -e "\033[31m !!!洞穴正在运行中!!! \033[0m"
 	fi
 }
 
 go(){
 	goMaster
 	goCaves
+}
+
+restart(){
+	stop
+	go
 }
 
 reset(){
@@ -71,22 +68,23 @@ updst(){
 	stop
 	~/steamcmd/steamcmd.sh +login anonymous +force_install_dir ~/dst +app_update 343050 validate +quit
 	if [[ `echo $?` -eq 0 ]]; then
-		echo -e "\033[46;37m ##: Dst game update to new sucsess! \033[0m"
+		echo -e "\033[46;37m ##: 饥荒游戏版本更新成功!! \033[0m"
 	fi
 }
 
 main(){
 
-	echo -e "\033[42;30m ### what do you want to do? ### \033[0m"
-	echo -e "\033[32m 0. \033[0m start master and cave server"
-	echo -e "\033[32m 1. \033[0m only start master server"
-	echo -e "\033[32m 2. \033[0m only start cave server"
-	echo -e "\033[32m 3. \033[0m stop running server"
-	echo -e "\033[32m 4. \033[0m delete server's saved file"                                                                                                 
-	echo -e "\033[32m 5. \033[0m restart dst server"
-	echo -e "\033[32m 6. \033[0m update dst game"
+	echo -e "\033[42;30m ### 来做个选 ♂ 择吧 (输入数字,回车确认) ### \033[0m"
+	echo -e "\033[32m 0. \033[0m 启动地上+洞穴"
+	echo -e "\033[32m 1. \033[0m 只启动地上"
+	echo -e "\033[32m 2. \033[0m 只启动洞穴"
+	echo -e "\033[32m 3. \033[0m 停止饥荒游戏进程"
+	echo -e "\033[32m 4. \033[0m 删除游戏存档记录"                                                                                             
+	echo -e "\033[32m 5. \033[0m 重启游戏(非重置),可以更新mod"
+	echo -e "\033[32m 6. \033[0m 重置饥荒游戏,将删除游戏存档记录"
+	echo -e "\033[32m 7. \033[0m 更新饥荒游戏版本"
 
-	read -p "Input your choose number: " choose
+	read -p "输入数字,回车确认选择 " choose
 	# if [[ $choose -gt 0 ]]; then
 		case $choose in
 			0 ) go
@@ -99,11 +97,13 @@ main(){
 				;;
 			4 ) del
 				;;
-			5 ) reset
+			5 ) restart
 				;;
-			6 ) updst
+			6 ) reset
 				;;
-			* ) echo -e "\033[31mPlease enter the number before the following options!! \033[0m"
+			7 ) updst
+				;;
+			* ) echo -e "\033[31m 请输入下列正确的数字选项!! \033[0m"
 				main
 				;;
 		esac
