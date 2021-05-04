2603705a3c6d7d67366011846ecb6835d0d39763
Butter fly <super771783057@foxmail.com>
2021-05-04 22:54:10 +0800

    fix

--- getRelease.py
+++ getRelease.py
@@ -5,9 +5,6 @@ from lxml import etree
 def getLastRelease(url='https://forums.kleientertainment.com/game-updates/dst/'):
     content=etree.HTML(requests.get(url).text)
     updates=content.xpath('//h3[contains(@class,"ipsType_sectionHead")]')
-    # release_list=[int(r.strip()) for r in content.xpath('//h3[@class="ipsType_sectionHead ipsType_break"]/text()') if not r.isspace()]
-    # res=max(release_list)
-
     releases=[ r.xpath('text()')[0].strip() for r in updates if not r.xpath('span[contains(@class,"negative")]')]
     print(max(releases))
     return max(releases)
